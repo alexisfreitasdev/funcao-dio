f7444234855de304f5c98eb78622bb6f0248e7e1
alexisfreitasdev <alexisfreitas.dev@outlook.com>
2026-03-07 21:53:01 +0000

    Minha Primeira Aula de Função

---
+++ funcao.js
@@ -0,0 +1,19 @@
+function torrar(){
+console.log ("Torrando pão");
+
+}
+
+function injetarPao(){
+ console.log ('Injetando pão');
+
+}
+
+function Geleia(){
+  console.log ("Adicionando Geleia");
+    console.log ("Finalizado")
+
+}
+
+torrar()
+injetarPao()
+Geleia()
