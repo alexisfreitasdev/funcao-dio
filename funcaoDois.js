7ac398511fafbd2ee3e42d367b2e08df3a201564
alexisfreitasdev <alexisfreitas.dev@outlook.com>
2026-03-07 22:32:17 +0000

    Exercicio 2

---
+++ funcaoDois.js
@@ -0,0 +1,29 @@
+function getData(){
+    console.log ("Pegando dados do cliente");
+}
+    function checkValues(){
+        console.log ("Checando CPF do usuario");
+        console.log ("Checando credencial do usuario");
+        console.log ("Checando RG do usuario");
+    
+    }
+        function sendToDataBase(){
+            console.log ("Enviando Dados para o sistema");
+        
+
+
+        }
+
+            function checkout(){
+                console.log ("Acesso Liberado");
+            }
+                function main(){
+                    getData()
+                    checkValues()
+                    sendToDataBase()
+                    checkout()
+
+
+                }
+                    main()
+                
