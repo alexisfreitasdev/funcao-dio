53a6b0b00eb5472000d1aebe852e314a8bc9178d
alexisfreitasdev <alexisfreitas.dev@outlook.com>
2026-03-08 10:38:55 +0000

    Função com Parâmetro

--- funcaocomparametro.js
+++ funcaocomparametro.js
@@ -1,5 +1,6 @@
-function torra(pao){
+function torra(pao, nome){
   console.log("Sua torrada está pronta com " + pao)
+  console.log("Ela é um pedido de " + nome)
 }
 
-torra("pão integral e pasta de amendoim")
+torra("pão integral e pasta de amendoim", "Felipe")
