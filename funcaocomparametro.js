17cd3c4bce2076a74d158aabac271a4c8fd06145
alexisfreitasdev <alexisfreitas.dev@outlook.com>
2026-03-08 10:47:43 +0000

    Função com parâmertro

--- funcaocomparametro.js
+++ funcaocomparametro.js
@@ -6,7 +6,7 @@ function torra(pao, nome){
 
 torra("pão integral e pasta de amendoim", "Felipe")
 
-// se não houver nome atribuido  , podemos atribuir um nome padão na função
+// se não houver nome atribuido  , podemos atribuir um nome padrão na função
 function torra(pao, nome = "Cliente"){
     console.log("Sua torrada está pronta com " + pao)
     console.log("Ela é um pedido de " + nome)
